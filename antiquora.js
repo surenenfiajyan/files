50dca4fba59a6cbeefcbac2c897bdcd0cd465641
Suren Enfiajyan <surenenfiajyan@gmail.com>
2022-01-06 23:31:21 +0400

    Update and rename ub-script-2.js to antiquora.js

--- antiquora.js
+++ antiquora.js
@@ -1,12 +1,10 @@
 /// antiquora.js
 console.log('started antiquora.js');
 var counter = 4;
-
 var interval = setInterval(() => {
   document.querySelectorAll('*').forEach(l => l.style = '')
   var s = document.documentElement.innerHtml;
   document.documentElement.innerHtml = s;
-  
   if (--counter < 1) {
     clearInterval(interval);
   }
