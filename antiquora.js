1f3d19c750eb4a2412ee65107a76d81f5897edef
Suren Enfiajyan <surenenfiajyan@gmail.com>
2022-01-06 23:04:05 +0400

    Delete antiquora.js

--- antiquora.js
+++
@@ -1,7 +0,0 @@
-/// antiquora.js
-setTimeout(() => {
-  document.querySelectorAll('*').forEach(l => l.style = '')
-  var s = document.documentElement.innerHtml;
-  document.documentElement.innerHtml = s;
-  console.log('quora is unlocked');
-}, 500);
