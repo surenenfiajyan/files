bfd6dcb74fade11e1711425d9d3135f13ba629f0
Suren Enfiajyan <surenenfiajyan@gmail.com>
2022-01-06 23:14:02 +0400

    Update and rename ub-script-1.js to ub-script-2.js

--- ub-script-2.js
+++ ub-script-2.js
@@ -1,11 +1,12 @@
 /// antiquora.js
 
+console.log('started antiquora.js');
+
 var counter = 4;
 var interval = setInterval(() => {
   document.querySelectorAll('*').forEach(l => l.style = '')
   var s = document.documentElement.innerHtml;
   document.documentElement.innerHtml = s;
-  console.log('quora is unlocked');
   
   if (--counter < 1) {
     clearInterval(interval);
