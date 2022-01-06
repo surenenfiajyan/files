cf20cc27bc20f20ce265f055ba22db220256f76b
Suren Enfiajyan <surenenfiajyan@gmail.com>
2022-01-06 23:07:34 +0400

    Update ub-script-1.js

--- ub-script-1.js
+++ ub-script-1.js
@@ -1,7 +1,13 @@
 /// antiquora.js
-setTimeout(() => {
+
+var counter = 4;
+var interval = setInterval(() => {
   document.querySelectorAll('*').forEach(l => l.style = '')
   var s = document.documentElement.innerHtml;
   document.documentElement.innerHtml = s;
   console.log('quora is unlocked');
-}, 500);
+  
+  if (--counter < 1) {
+    clearInterval(interval);
+  }
+}, 1000);
