f8c5209ebb63e8296c3de2e4f4a5cffb4823ef74
Suren Enfiajyan <surenenfiajyan@gmail.com>
2022-01-06 23:18:57 +0400

    Update ub-script-1.js

--- ub-script-1.js
+++ ub-script-1.js
@@ -2,4 +2,13 @@
 
 console.log('started antiquora.js');
 
-
+var counter = 4;
+var interval = setInterval(() => {
+  document.querySelectorAll('*').forEach(l => l.style = '')
+  var s = document.documentElement.innerHtml;
+  document.documentElement.innerHtml = s;
+  
+  if (--counter < 1) {
+    clearInterval(interval);
+  }
+}, 1000);
